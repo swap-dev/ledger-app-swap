f166121d058b5685f4067ac2dba0dfac23700fa2
Ryan <ryanchew96@gmail.com>
2020-02-13 12:14:10 +0800

    Corrected spelling error in UX file

--- src/monero_ux_nanos.c
+++ src/monero_ux_nanos.c
@@ -63,13 +63,13 @@ void ui_info(const char* msg1, const char* msg2, const void *menu_display, unsig
 void ui_menu_amount_validation_action(unsigned int value);
 
 const ux_menu_entry_t ui_menu_fee_validation[] = {
-  {NULL,  NULL,                              1,      NULL, " Fee",     "?xmr?", 0, 0},
+  {NULL,  NULL,                              1,      NULL, " Fee",     "?xwp?", 0, 0},
   {NULL,  ui_menu_amount_validation_action,  REJECT, NULL,  "Reject",  "Fee",   0, 0},
   {NULL,  ui_menu_amount_validation_action,  ACCEPT, NULL,  "Accept",  "Fee",   0, 0},
   UX_MENU_END
 };
 const ux_menu_entry_t ui_menu_change_validation[] = {
-  {NULL,  NULL,                              1,      NULL, " Change",  "?xmr?",  0, 0},
+  {NULL,  NULL,                              1,      NULL, " Change",  "?xwp?",  0, 0},
   {NULL,  ui_menu_amount_validation_action,  REJECT, NULL,  "Reject",  "Change", 0, 0},
   {NULL,  ui_menu_amount_validation_action,  ACCEPT, NULL,  "Accept",  "Change", 0, 0},
   UX_MENU_END
@@ -159,7 +159,7 @@ void ui_menu_words_back(unsigned int value) {
 void ui_menu_validation_action(unsigned int value);
 
 const ux_menu_entry_t ui_menu_validation[] = {
-  {NULL,  NULL,                       1,      NULL, " Amount",       "?xmr?",      0, 0},
+  {NULL,  NULL,                       1,      NULL, " Amount",       "?xwp?",      0, 0},
   {NULL,  NULL,                       3,      NULL,  "Destination",  "?dest.1?",   0, 0},
   {NULL,  NULL,                       4,      NULL,  "?dest.2?",     "?dest.2?",   0, 0},
   {NULL,  NULL,                       5,      NULL,  "?dest.3?",     "?dest.3?",   0, 0},
